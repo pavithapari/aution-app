06001ee6d479d99156eea324b426e26f1a138546
Pavitha P <pavithapari10@gmail.com>
2025-09-02 18:24:19 +0530

    created basic app structure

---
+++ app/main/routes.py
@@ -0,0 +1,13 @@
+from flask import render_template,request
+from flask import Flask
+from flask import Blueprint
+
+
+main=Blueprint('main', __name__)
+
+@main.route('/')
+def home():
+    return render_template('base.html')
+
+
+
