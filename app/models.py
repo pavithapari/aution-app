37878ce27798db06c9ed47d7cf14374d4563529a
RAGASUDHA-B <sudha.balu.471@gmail.com>
2025-09-06 01:15:02 +0530

    updated the app/models.py

--- app/models.py
+++ app/models.py
@@ -31,6 +31,7 @@ Bid
 * `bidder_id` (FK → User.id)
 * `auction_id` (FK → Auction.id)
 """
+from werkzeug.security import generate_password_hash,check_password_hash
 from app import db
 
 
@@ -45,3 +46,17 @@ class Sellers(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(120), nullable=False)
+
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(20),nullable=False,unique=True)
+    email=db.Column(db.String(120),nullable=False,unique=True)
+    password_hash=db.Column(db.tString(128),nullable=False)
+    role=db.Column(db.String(10),nullable=False)
+
+
+    def set_password(self,password):
+        self.password_hash=generate_password_hash(password)
+
+    def check_password(self,password):
+        return check_password_hash(self.password_hash,password)
